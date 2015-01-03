e50743dde42b2871415fbcafefb94eb2909084d1
Mike Wilcox <mwilcox56@gmail.com>
2015-01-03 14:28:38 -0500

    dont need to new up

--- scripts/utils/web-api.js
+++ scripts/utils/web-api.js
@@ -1,14 +1,10 @@
 import $ from 'jquery';
-import Promise from 'es6-promise';
+import { Promise } from 'es6-promise';
 
 export default class WebApi {
 
-  constructor() {
-    this.promise = Promise.Promise;
-  }
-
   initData() {
-    return this.promise.resolve($.ajax('http://localhost:3001/api/books'));
+    return Promise.resolve($.ajax('http://localhost:3001/api/books'));
   }
 
 }
