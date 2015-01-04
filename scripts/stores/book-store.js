7342bf7d4ebac780179cdc734c12d2b3dcb3d4a3
Mike Wilcox <mwilcox56@gmail.com>
2015-01-03 21:09:43 -0500

    add the book store

---
+++ scripts/stores/book-store.js
@@ -0,0 +1,51 @@
+import Dispatcher from '../dispatcher/book-dispatcher';
+import Constants from '../constants/constants';
+import Events from 'eventemitter3';
+
+class BookStore extends Events.EventEmitter {
+
+  constructor() {
+    super();
+    Dispatcher.register(this._dispatchToken.bind(this));
+    this.CHANGE_EVENT = 'change';
+    this._books = [];
+  }
+
+  addChangeListener(callback) {
+    this.on(this.CHANGE_EVENT, callback);
+  }
+
+  removeChangeListener(callback) {
+    this.removeListener(this.CHANGE_EVENT, callback);
+  }
+
+  getBooks() {
+    return this._books;
+  }
+
+  _emitChange() {
+    this.emit(this.CHANGE_EVENT);
+  }
+
+  _setBooks(books) {
+    this._books = books;
+  }
+
+  _dispatchToken(payload) {
+    let action = payload.action;
+
+    switch(action.type) {
+
+      case Constants.ActionTypes.RECEIVE_BOOKS:
+        this._setBooks(action.books);
+        this._emitChange();
+        break;
+
+      default:
+        // do nothing
+    }
+  }
+}
+
+let bs = new BookStore();
+export default bs;
