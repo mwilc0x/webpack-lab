5c8b09808facab31ddfcaa766e7837f249e85155
Mike Wilcox <mwilcox56@gmail.com>
2015-01-03 21:10:53 -0500

    use promise to fetch data

--- scripts/utils/web-api.js
+++ scripts/utils/web-api.js
@@ -1,10 +1,16 @@
 import $ from 'jquery';
 import { Promise } from 'es6-promise';
+import Actions from '../actions/book-actions';
 
 export default class WebApi {
 
   initData() {
-    return Promise.resolve($.ajax('http://localhost:3001/api/books'));
+    let actions = new Actions();
+
+    Promise.resolve($.ajax('http://localhost:3001/api/books'))
+      .then((books) => {
+        actions.receiveBooks(books);
+      });
   }
 
 }
