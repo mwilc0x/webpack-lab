ec4fc6f2c7cd719f3415db9c3865051ddea5aacb
Mike Wilcox <mwilcox56@gmail.com>
2015-01-05 21:20:34 -0500

    add header component

---
+++ scripts/components/header.react.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Actions from '../actions/book-actions';
+
+export default React.createClass({
+
+  _filter(event) {
+    Actions.filterBooks(event.target.value.toLowerCase());
+  },
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Webpack Lab</a>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <form className="navbar-form navbar-left" role="search">
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Search" onChange={this._filter} />
+                </div>
+              </form>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+    );
+  }
+
+});
