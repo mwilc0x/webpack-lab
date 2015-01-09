717802103206e55091624955df60e3b5a3d77c42
Mike Wilcox <mwilcox56@gmail.com>
2015-01-08 19:49:06 -0500

    add routing w/ react-router

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,26 @@
 import * as React from 'react';
-import Utils from './utils/web-api';
+import * as Router from 'react-router';
 import Header from './components/header.react';
-import BookList from './components/book-list.react';
-import BookStore from './stores/book-store';
+import Home from './components/home.react';
 
-export default React.createClass({
+let { DefaultRoute, Route, RouteHandler } = Router;
 
-  getInitialState() {
-    return { lists: [] };
-  },
-
-  shouldComponentUpdate() {
-    return true;
-  },
-
-  componentDidMount() {
-    let utils = new Utils();
-    utils.initData();
-
-    BookStore.addChangeListener(this._onChange);
-  },
-
-  _getBooksFromStore() {
-      this.setState( { lists: BookStore.getBooks() });
-  },
-
-  _onChange() {
-    this._getBooksFromStore();
-  },
-
-  _renderBookList(list, index) {
-    return <BookList
-      key={index}
-      books={list.books}
-      date={list.date} title={list.title}
-    />
-  },
+let App = React.createClass({
 
   render() {
     return (
       <div className="container">
         <Header />
-        { !this.state.lists.length ? 'Loading' : this.state.lists.map(this._renderBookList) }
+        <RouteHandler/>
       </div>
     );
   }
 
 });
+
+var routes = <Route handler={App}>
+  <DefaultRoute name="home" handler={Home}/>
+</Route>
+Router.run(routes, function(Handler) {
+  React.render(<Handler/>, document.body)
+})
