b98f55e8e8c1fc57b9e0fceafaccf12b40410c20
Mike Wilcox <mwilcox56@gmail.com>
2015-01-08 19:47:55 -0500

    add home component

---
+++ scripts/components/home.react.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Utils from '../utils/web-api';
+import BookStore from '../stores/book-store';
+import BookList from '../components/book-list.react';
+
+export default React.createClass({
+
+  getInitialState() {
+    return { lists: [] };
+  },
+
+  shouldComponentUpdate() {
+    return true;
+  },
+
+  componentDidMount() {
+    let utils = new Utils();
+    utils.initData();
+
+    BookStore.addChangeListener(this._onChange);
+  },
+
+  _getBooksFromStore() {
+    this.setState( { lists: BookStore.getBooks() });
+  },
+
+  _onChange() {
+    this._getBooksFromStore();
+  },
+
+  _renderBookList(list, index) {
+    return <BookList
+    key={index}
+    books={list.books}
+    date={list.date} title={list.title}
+    />
+  },
+
+  render() {
+    return (
+      <div>
+        { !this.state.lists.length ? 'Loading' : this.state.lists.map(this._renderBookList) }
+      </div>
+    );
+  }
+
+});
