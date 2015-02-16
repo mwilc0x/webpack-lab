ba3031b365eabbbb497acb177afd05c66feb3640
Mike Wilcox <mwilcox56@gmail.com>
2015-02-16 09:28:30 -0500

    add toggle and animation using ReactCSSTransitionGroup

--- scripts/components/book-list.react.js
+++ scripts/components/book-list.react.js
@@ -1,7 +1,14 @@
-import * as React from 'react';
+import * as React from 'react/addons';
 import Book from './book.react';
+import Style from '../utils/styles';
+
+let ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 export default React.createClass({
+
+  getInitialState() {
+    return { show: true };
+  },
 
   propTypes: {
     books: React.PropTypes.array.isRequired,
@@ -10,18 +17,41 @@ export default React.createClass({
   },
 
   renderBook(book) {
-    return <Book
-      key={book.id}
-      index={book.index}
-      summary={book.summary}
-    />
+    return (
+      <div className="col-md-12">
+        <Book
+          key={book.id}
+          index={book.index}
+          summary={book.summary}
+        />
+      </div>
+    )
+  },
+
+  _toggleList() {
+    this.setState({show: !this.state.show })
   },
 
   render() {
+
+    let bookList = null;
+
+    if(this.state.show)
+      bookList = <div key={this.props.title} className="row">{this.props.books.map(this.renderBook)}</div>;
+
     return (
       <div id="book-list">
-        <h1>{this.props.title}</h1>
-        { this.props.books.map(this.renderBook) }
+        <div className="row">
+          <div className="col-md-12">
+            <h3 style={Style.app.inlineBlock}>{this.props.title}</h3>
+            <h6 style={Style.app.inlineBlock} onClick={ this._toggleList }>
+              { this.state.show ? 'collapse' : 'expand'  }
+            </h6>
+          </div>
+        </div>
+        <ReactCSSTransitionGroup transitionName="example">
+          {bookList}
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
